6bb4711cd3ea8f39d1786a19500751b49b213cfd
Rosilene4 <129714743+Rosilene4@users.noreply.github.com>
2025-06-20 10:33:42 -0300

    Ajuste nos caminhos dos arquivos no app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const sequelize = require('./models/database');
-const Livro = require('./models/Livro');
-const livrosRoutes = require('./routes/livros');
+const sequelize = require('./database');
+const Livro = require('./Livro');
+const livrosRoutes = require('.livros');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.render('index');
